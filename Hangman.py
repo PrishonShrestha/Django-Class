c99cd1849c8dcc52ceae4ef49a45a08e21ce646b
Prishon <prishonstha@gmail.com>
2022-07-28 20:12:25 +0545

    updated hangman

--- Hangman.py
+++ Hangman.py
@@ -5,31 +5,30 @@ def Hangman():
 
         correct_input =[]
         incorrect_input = []
-        print(word)
+        #print(word)
 
         
         while len(correct_input) != len(word):
             if len(incorrect_input) != 5:
-                player_guess = str(input("Enter a letter: "))
+                player_guess = str(input("Guess a letter: "))
                 #for player_guess in word:
                 if player_guess in word:
                     print("Correct")
                     if player_guess not in correct_input:
 
-
                         c=word.count(player_guess)
-                        for i in range(0,c):
+                        for i in range(0,c):  #Insert duplicate letters
                             correct_input.append(player_guess)
                         print("Correct Guesses: ", correct_input)
-
 
                     else:
                         print("Already guessed")
                 else:
-                    print("Incorrect. Please try again")
+                    print("Incorrect")
                     if player_guess not in incorrect_input:
                         incorrect_input.append(player_guess)
-                        print("Incorrect Guesses: ", incorrect_input)
+                        #print("Incorrect Guesses: ", len(incorrect_input))
+                        print("Number of guesses left: ", (5-len(incorrect_input)))
                     else:
                         print("Already guessed")
             else:
@@ -47,6 +46,7 @@ def Hangman():
                 break
         else:
             print("Congratulation")
+            print(user_input)
 
 
 
